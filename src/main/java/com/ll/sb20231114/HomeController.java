c2dabeb6a5ec2c7fa7a2b64a38976f15e71f5fc2
caaano <wlsrl0515@naver.com>
2023-11-14 15:45:04 +0900

    calc6 ~ calc13

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -1,40 +1,43 @@
 package com.ll.sb20231114;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
-// 컨트롤러 입니다.
 public class HomeController {
+
     @GetMapping("/")
     @ResponseBody
-        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
+        // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
     String showMain() {
-        System.out.println("안녕하세요!!!"); // 이 메세지는 콘솔에 출력됨, 클라이언트에게 전송되지 않습니다.
+        System.out.println("안녕하세요!!!"); // 이 메시지는 콘솔에 출력됨, 클라이언트에게 전송되지 않습니다.
         return "안녕하세요.";
     }
 
     @GetMapping("/about")
     @ResponseBody
-        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
     String showAbout() {
         return "개발자 커뮤니티";
     }
 
     @GetMapping("/calc")
     @ResponseBody
-        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
     String showCalc(int a, int b) {
         return "계산 결과 : %d".formatted(a + b);
     }
 
     @GetMapping("/calc2")
     @ResponseBody
-        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
     String showCalc2(Integer a, Integer b) {
-        return "a: " + a + ",  b :" + b;
+        return "a : " + a + ", b : " + b;
     }
 
     @GetMapping("/calc3")
@@ -63,4 +66,97 @@ public class HomeController {
     ) {
         return "계산 결과 : %s".formatted(a + b);
     }
+
+    @GetMapping("/calc6")
+    @ResponseBody
+    int showCalc6(
+            int a, int b
+    ) {
+        return a + b;
+    }
+
+    @GetMapping("/calc7")
+    @ResponseBody
+    boolean showCalc7(
+            int a, int b
+    ) {
+        return a > b;
+    }
+
+    @GetMapping("/calc8")
+    @ResponseBody
+    Person showCalc8(
+            String name, int age
+    ) {
+        return new Person(name, age);
+    }
+
+    @GetMapping("/calc9")
+    @ResponseBody
+    Person2 showCalc9(
+            String name, int age
+    ) {
+        return new Person2(name, age);
+    }
+
+    @GetMapping("/calc10")
+    @ResponseBody
+    Map<String, Object> showCalc10(
+            String name, int age
+    ) {
+        Map<String, Object> personMap = Map.of(
+                "name", name,
+                "age", age
+        );
+
+        return personMap;
+    }
+
+    @GetMapping("/calc11")
+    @ResponseBody
+    List<Integer> showCalc11() {
+        List<Integer> nums = new ArrayList<>() {{
+            add(10);
+            add(-510);
+            add(10010);
+        }};
+
+        return nums;
+    }
+
+    @GetMapping("/calc12")
+    @ResponseBody
+    int[] showCalc12() {
+        int[] nums = new int[]{10, -510, 10010};
+
+        return nums;
+    }
+
+    @GetMapping("/calc13")
+    @ResponseBody
+    List<Person2> showCalc13(
+            String name, int age
+    ) {
+        List<Person2> persons = new ArrayList<>() {{
+            add(new Person2(name, age));
+            add(new Person2(name + "!", age + 1));
+            add(new Person2(name + "!!", age + 2));
+        }};
+
+        return persons;
+    }
+}
+
+@AllArgsConstructor
+class Person {
+    public String name;
+    public int age;
+}
+
+@AllArgsConstructor
+class Person2 {
+    @Getter
+    private String name;
+    @Getter
+    private int age;
 }
