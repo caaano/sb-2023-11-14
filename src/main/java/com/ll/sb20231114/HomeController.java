1188dbd753cf3f17c4cb3d7fae1c9e879537add2
caaano <wlsrl0515@naver.com>
2023-11-14 13:35:36 +0900

    cal3 ~ cal5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,21 +2,65 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 // 컨트롤러 입니다.
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
+    @ResponseBody
+        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
     String showMain() {
         System.out.println("안녕하세요!!!"); // 이 메세지는 콘솔에 출력됨, 클라이언트에게 전송되지 않습니다.
         return "안녕하세요.";
     }
 
     @GetMapping("/about")
-    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
+    @ResponseBody
+        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
     String showAbout() {
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
+    String showCalc(int a, int b) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+        // 이 함수의 리턴값을 그대로 브라우저에 출력하라는 의미
+    String showCalc2(Integer a, Integer b) {
+        return "a: " + a + ",  b :" + b;
+    }
+
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산 결과 : %f".formatted(a + b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b
+    ) {
+        return "계산 결과 : %s".formatted(a + b);
     }
 }
